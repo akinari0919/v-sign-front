a98ca69fdefd21e4096b4e11993e40c021cb67cb
akinari <a.mizo0919@gmail.com>
2022-04-17 22:57:29 +0900

    add:コメントを追記 #1

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+// expressの呼び込み
 const express = require('express');
+// PORTの設定
+// もしenvにportが確立したらそのポートを、していなかったら8080を使う
 const port = process.env.PORT || 8080;
+// express()のインスタンス
 const app = express();
+// どこにアクセスさせるかの設定
+// distフォルダ：コンパイルされたファイルが入る場所
 app.use(express.static(__dirname + "/dist/"));
+// 一番上で指定したportをlisten
 app.listen(port);
