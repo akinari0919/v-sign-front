5850d8af42cbaf0cd4fff331fafdee19fd3b2d9d
akinari <a.mizo0919@gmail.com>
2022-05-05 15:58:37 +0900

    add:ページネーション設定

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -14,6 +14,14 @@
           現在{{ rankers }}位まで登録されています。
         </p>
 
+        <!-- ページネーション -->
+        <v-pagination
+          color="black"
+          v-model="page"
+          :length="length"
+          @input = "pageChange"
+        />
+
         <div class="body-1 mt-8 mb-1">
           <v-row>
             <v-col>
@@ -35,39 +43,49 @@
         </div>
 
         <v-card>
-        <div
-          class="mb-4"
-          v-for="(sign, index) in signs"
-          :key="index"
-        >
-          <v-row class="text-h6" align="center">
-            <v-col>
-              {{ index + 1 }}位
-            </v-col>
-            <v-col>
-              {{ sign.name }}
-            </v-col>
-            <v-col>
-              {{ sign.angle.toFixed(2) }}°
-            </v-col>
-            <v-col>
-              <img class="mb-1"
-                 :src="sign.image"
-                 width="150"
-                 height="150"
-              />
-            </v-col>
-            <v-col>
-              {{ sign.created_at.substr(0, 10) }}
-            </v-col>
-          </v-row>
-          <v-divider/>
-        </div>
+          <div
+            class="mb-4"
+            v-for="(sign, index) in displayLists"
+            :key="index"
+          >
+            <v-row class="text-h6" align="center">
+              <v-col>
+                {{ index + 1 + rank }}位
+              </v-col>
+              <v-col>
+                {{ sign.name }}
+              </v-col>
+              <v-col>
+                {{ sign.angle.toFixed(2) }}°
+              </v-col>
+              <v-col>
+                <img class="mb-1"
+                   :src="sign.image"
+                   width="150"
+                   height="150"
+                />
+              </v-col>
+              <v-col>
+                {{ sign.created_at.substr(0, 10) }}
+              </v-col>
+            </v-row>
+            <v-divider/>
+          </div>
         </v-card>
+
+        <!-- ページネーション -->
+        <v-pagination
+          class="mt-12"
+          color="black"
+          v-model="page"
+          :length="length"
+          @input = "pageChange"
+        />
 
       </v-col>
     </v-row>
   </v-container>
+
 </template>
 
 <script>
@@ -78,7 +96,13 @@ export default {
   data () {
     return {
       signs: null,
-      rankers: null
+      rankers: null,
+      // ページネーション
+      rank: 0,
+      length: 0,
+      page: 1,
+      displayLists: [],
+      pageSize: 10,
     }
   },
 
@@ -87,11 +111,26 @@ export default {
       .get(`${url}/v1/signs`)
       .then( response => {
         this.signs = response.data.signs
+
+        // ページネーション
+        this.length = Math.ceil(this.signs.length/this.pageSize)
+        this.displayLists = this.signs.slice(this.pageSize*(this.page -1),
+                            this.pageSize*(this.page))
+        // 登録数
         this.rankers = response.data.rankers
       })
       .catch( (err) => {
         this.msg = err // エラー処理
       });
+  },
+
+  // ページネーション
+  methods: {
+    pageChange: function(pageNumber){
+      this.displayLists = this.signs.slice(this.pageSize*(pageNumber -1),
+                          this.pageSize*(pageNumber))
+      this.rank = pageNumber *10 -10
+    }
   }
 }
 </script>
